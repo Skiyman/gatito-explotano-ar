2d8b6595164d2116f239198ec3c85f7c3628ab87
nyabine <72140992+nyabine@users.noreply.github.com>
2025-03-26 13:37:20 +0700

    добавлен дрободык, смена режима стрельбы, урон, хп придуркам

--- Assets/Scripts/GatitoScript.cs
+++ Assets/Scripts/GatitoScript.cs
@@ -5,6 +5,12 @@ using UnityEngine;
 public class GatitoScript : MonoBehaviour
 {
     public GameObject explode;
+    public float maxHealth = 100f;
+    private float currentHealth;
+
+    void Start () {
+        currentHealth = maxHealth;
+    }
 
     void Update()
     {
@@ -13,16 +19,31 @@ public class GatitoScript : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, cameraPos, 0.5f * Time.deltaTime);
     }
 
-    void OnTriggerEnter(Collider bullet)
-    {
+    void Die() {
         BoxCollider gatito = gameObject.GetComponent<BoxCollider>();
         Vector3 hitPoint = gatito.ClosestPoint(transform.position);
         Vector3 hitNormal = gatito.transform.forward;
 
         GameObject explodeInstance = Instantiate(explode, hitPoint, Quaternion.LookRotation(hitNormal));
 
-        Destroy(gatito.gameObject);
-        Destroy(bullet.gameObject);
         Destroy(explodeInstance, 0.8f);
+        Destroy(gatito.gameObject);
+    }
+
+    public void TakeDamage(float damage) {
+        currentHealth -= damage;
+
+        if (currentHealth <= 0) {
+            Die();
+        }
+    }
+
+    void OnTriggerEnter(Collider bullet)
+    {
+        BulletScript bulletScript = bullet.GetComponent<BulletScript>();
+        if (bullet != null) {
+            TakeDamage(bulletScript.damage);
+        }
+        Destroy(bullet.gameObject);
     }
 }
